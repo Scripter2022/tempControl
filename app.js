72a2e8c5cb32e6ca8b1276dcb1daa6897b702441
Alex Chichkarev <117197132+Scripter2022@users.noreply.github.com>
2023-12-18 22:12:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,21 +9,15 @@ var testRouter = require("./routes/test");
 var mq = require("./lib/mq.js");
 var app = express();
 
-
 mq.mq();
 
 var {tempHot}=require('./lib/mq.js');
-
 console.log("From main Script",tempHot)
-
 console.log("variable is define!");
-
 //const http=require('http');
 
-app.listen(3000); 
-
+app.listen(80); 
 // view engine setup
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
@@ -32,7 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "lib")));
-
 
 console.log(tempHot);
 
@@ -54,16 +47,11 @@ app.get('/data', (req, res) => {
     res.end();
  
  })
-
 // catch 404 and forward to error handler
-
 app.use(function (req, res, next) {
-
   next(createError(404));
-
 });
-
-// error handler
+ handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -72,7 +60,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
 module.exports = app;
 
 
